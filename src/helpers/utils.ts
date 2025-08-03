b10a93cda261c2c5234bca9ec41350e963340b26
sanketpandia <sanketpandia@gmail.com>
2025-08-04 03:17:55 +0530

    Dockerfile for image UI

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -4,8 +4,8 @@ const API_KEY = process.env.API_KEY ?? "123"
 
 export function generateMetaHeaders (metainfo: MetaInfo) {
     return {
-        "X-Discord-Id": metainfo.discordId,
-        "X-Server-Id": metainfo.userId,
+        "X-Discord-Id": metainfo.userId,
+        "X-Server-Id": metainfo.discordId,
         "X-API-Key": API_KEY,
         "Content-Type": "application/json"
     }
